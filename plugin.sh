3558b1105444cd4f8d078534c39f533508ade7ff
Alexander Shevchenko <kudato@me.com>
2019-06-17 04:14:51 +0300

    Add use '--password-stdin' to 'docker login' command.

--- plugin.sh
+++ plugin.sh
@@ -14,7 +14,7 @@ check_plugin_vars() {
     for e in \
         PLUGIN_ACCESS_KEY,AWS_ACCESS_KEY_ID \
         PLUGIN_SECRET_KEY,AWS_SECRET_ACCESS_KEY \
-        PLUGIN_REGION,AWS_DEFAULT_REGION
+        PLUGIN_REGION,AWS_REGION
     do
         set -- $e
         check_plugin_param "$1" "$2"
@@ -26,29 +26,44 @@ pull_counter() {
     export PULL_IMAGES="$1 ${PULL_IMAGES}"
 }
 
-if [ ! -S "/var/run/docker.sock" ]; then
-    echo "Docker is not running"
+docker_login() {
+    if [ ! -S "/var/run/docker.sock" ]; then
+        echo "Docker is not running"
+        exit 1
+    else
+        check_plugin_vars
+        if [ -z "${AWS_REGION}" ]; then
+            export AWS_REGION=${AWS_DEFAULT_REGION}
+        fi
+        local auth registry password
+        auth=$(aws ecr get-login --no-include-email \
+                            --region "${AWS_REGION}")
+
+        registry=${auth##* }
+        auth=${auth% *}
+        password=${auth##* }
+        echo "$password" | docker login -u AWS \
+                            --password-stdin "$registry"
+    fi
+}
+
+if ! docker_login
+then
+    echo "Seems something went wrong."
     exit 1
 else
-    check_plugin_vars
-    if [ -z "${AWS_DEFAULT_REGION}" ]; then
-        export AWS_DEFAULT_REGION=us-east-1
-    fi
-    $(aws ecr get-login --no-include-email --region "${AWS_DEFAULT_REGION}")
-fi
+    OLDIFS=$IFS
+    while IFS=',' read -ra IMAGES; do
+        for i in "${IMAGES[@]}"; do
+            docker pull $i &
+            pull_counter $!
+        done
+    done <<< "$PLUGIN_IMAGES"
+    IFS=$OLDIFS
 
-OLDIFS=$IFS
-while IFS=',' read -ra IMAGES; do
-    for i in "${IMAGES[@]}"; do
-        docker pull $i &
-        pull_counter $!
+    for i in ${PULL_IMAGES}
+    do
+        wait "$i"
     done
-done <<< "$PLUGIN_IMAGES"
-IFS=$OLDIFS
-
-for i in ${PULL_IMAGES}
-do
-    wait "$i"
-done
-
+fi
 exit 0
